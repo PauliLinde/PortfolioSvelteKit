5361478ca4e8a4e560297c6609078b3fa0fe13c1
PauliLinde <jus.paulina@gmail.com>
2025-09-23 14:31:12 +0200

    Filled frontpage with basic information about my page/portfolio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+	let name = 'Paulina Linderborg';
+	let profession = 'DEVELOPER';
+</script>
+
+<h3>Hello, my name is {name}, I'm a</h3>
+<h1>{profession}</h1>
+<h4>Welcome to my portfolio. Here you can find some more information about me,
+	my experience, you can see some of my projects, and you can can contact me if
+	your interested to work with me. So don't wait and check out more!
+</h4><br>
+<button>Find out more</button>
+
+
+
+
+
+
+
+
+
+
+<!--<h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 
@@ -9,4 +31,4 @@
 
 <div class="bg-red-500 text-white p-4 m-8 rounded-lg">
 	TEST: Om denna box är röd med vit text så fungerar Tailwind!
-</div>
+</div>-->
