6d152ec4de6aa5bb5d283e86d1f6f8c951adc088
PauliLinde <jus.paulina@gmail.com>
2025-09-24 13:54:28 +0200

    added .toUpperCase to profession

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	let name = 'Paulina Linderborg';
-	let profession = 'DEVELOPER';
+	let profession = 'developer';
 </script>
 
 <h3>Hello, my name is {name}, I'm a</h3>
-<h1>{profession}</h1>
+<h1>{profession.toUpperCase()}</h1>
 <h4>Welcome to my portfolio. Here you can find some more information about me,
 	my experience, you can see some of my projects, and you can can contact me if
 	your interested to work with me. So don't wait and check out more!
