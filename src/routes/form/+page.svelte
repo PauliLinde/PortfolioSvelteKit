2cd141e509ac2b52754ab4edf7a1f03131ffbcb5
PauliLinde <jus.paulina@gmail.com>
2025-10-06 12:57:51 +0200

    Adding text to form page

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -16,6 +16,14 @@
 
 <div class="min-h-screen flex items-center justify-center bg-gray-50 px-4 py-12">
 	<div class="w-full max-w-2xl bg-white rounded-lg shadow-md p-6 sm:p-8">
+
+		<div class="text-center mb-12">
+			<h3 class="text-xl md:text-2xl text-gray-600 mb-4">
+				Would you like to know more, have any questions, want to work together, or just tell a bad joke?
+				Please don't hesitate to send me a message!
+			</h3>
+		</div>
+
 		<form method="POST" action="/form">
 			<label for="Name" class="relative">
 				<input
@@ -24,13 +32,10 @@
 					name="name"
 					placeholder=""
 					class="peer mt-0.5 w-full rounded border-gray-300 shadow-sm sm:text-sm"
-					required
-				/>
-				<span
-					class="absolute inset-y-0 start-3 -translate-y-5 bg-white px-0.5 text-sm font-medium text-gray-700 transition-transform peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-5"
-				>
-				Name
-			</span>
+					required />
+				<span class="absolute inset-y-0 start-3 -translate-y-5 bg-white px-0.5 text-sm font-medium text-gray-700 transition-transform peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-5">
+					Name
+				</span>
 			</label><br>
 			<label for="Email" class="relative">
 				<input
@@ -39,38 +44,28 @@
 					name="email"
 					placeholder=""
 					class="peer mt-0.5 w-full rounded border-gray-300 shadow-sm sm:text-sm"
-					required
-				/>
-
-				<span
-					class="absolute inset-y-0 start-3 -translate-y-5 bg-white px-0.5 text-sm font-medium text-gray-700 transition-transform peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-5"
-				>
-				Email
-			</span>
+					required />
+				<span class="absolute inset-y-0 start-3 -translate-y-5 bg-white px-0.5 text-sm font-medium text-gray-700 transition-transform peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-5">
+					Email
+				</span>
 			</label><br>
 			<label for="Message" class="relative block">
-			<textarea
-				id="message"
-				rows="5"
-				name="message"
-				placeholder=""
-				class="peer mt-0.5 w-full rounded border-gray-300 shadow-sm sm:text-sm resize-y"
-				required
-			></textarea>
-
-				<span
-					class="absolute top-0 start-3 -translate-y-5 bg-white px-0.5 text-sm font-medium text-gray-700 transition-transform peer-placeholder-shown:translate-y-3 peer-focus:-translate-y-5"
-				>
-			 Message
-			</span>
+				<textarea
+					id="message"
+					rows="5"
+					name="message"
+					placeholder=""
+					class="peer mt-0.5 w-full rounded border-gray-300 shadow-sm sm:text-sm resize-y"
+					required>
+					</textarea>
+					<span class="absolute top-0 start-3 -translate-y-5 bg-white px-0.5 text-sm font-medium text-gray-700 transition-transform peer-placeholder-shown:translate-y-3 peer-focus:-translate-y-5">
+					 Message
+					</span>
 			</label>
 			<button
 				type="submit"
-				class="group inline-block rounded-full bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 p-[2px] hover:text-white focus:ring-3 focus:outline-hidden"
-						>
-				<span
-					class="block rounded-full bg-white px-8 py-3 text-sm font-medium group-hover:bg-transparent transition-all"
-				>
+				class="group inline-block rounded-full bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 p-[2px] hover:text-white focus:ring-3 focus:outline-hidden">
+				<span class="block rounded-full bg-white px-8 py-3 text-sm font-medium group-hover:bg-transparent transition-all">
 					Send
 				</span>
 			</button>
